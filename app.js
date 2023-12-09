251008cd0c27e8d2b5b80a030ae0c9d265015e4c
Ajay Duraisamy <ajayduraisamy@gmail.com>
2023-12-09 12:30:00 +0530

    Final secure auth dashboard with hashing, guard and auto logout

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const app = document.getElementById("app");
 
 let users = JSON.parse(localStorage.getItem("users")) || [];
 let session = localStorage.getItem("session");
+let sessionTimer = null;
 
-/* UI VIEWS */
+/* ================= UI ================= */
 
 function loginUI(){
  app.innerHTML = `
@@ -23,14 +24,15 @@ function loginUI(){
 function registerUI(){
  app.innerHTML = `
      <h4 class="text-center mb-3">Register</h4>
+
      <input id="ru" class="form-control mb-2" placeholder="Username">
      <input id="rp" type="password" class="form-control mb-3" placeholder="Password">
 
      <button class="btn btn-success w-100" onclick="register()">Register</button>
 
      <p class="text-center mt-3">
-      Have an account?
-      <span class="text-primary" style="cursor:pointer" onclick="loginUI()">Login</span>
+      Have account?
+      <span class="text-primary" style="cursor:pointer" onclick="loginUI()"> Login</span>
      </p>
  `;
 }
@@ -38,64 +40,105 @@ function registerUI(){
 function dashboardUI(user){
  app.innerHTML = `
     <h4 class="text-center">Dashboard</h4>
-    <p class="text-center">Welcome, <b>${user}</b></p>
+    <p class="text-center">Welcome <b>${user}</b></p>
 
-    <button class="btn btn-danger w-100 mt-3" onclick="logout()">Logout</button>
+    <p class="text-center text-muted">
+      Session expires automatically after inactivity
+    </p>
+
+    <button class="btn btn-danger w-100 mt-3" onclick="logout()">
+      Logout
+    </button>
  `;
 }
 
 
-/* LOGIC */
+/* ================= LOGIC ================= */
 
+function hash(p){
+ return btoa(p);   // base64 hash simulation
+}
+
+// Register
 function register(){
  const u = ru.value.trim();
  const p = rp.value.trim();
 
- if(u.length < 3) return alert("Username must be at least 3 characters");
- if(p.length < 6) return alert("Password must be at least 6 characters");
+ if(u.length < 3) return alert("Username must be 3+ characters");
+ if(p.length < 6) return alert("Password must be 6+ characters");
 
  if(users.some(x => x.username === u))
     return alert("User already exists");
 
- users.push({username:u, password:p});
+ users.push({
+    username: u,
+    password: hash(p)
+ });
+
  localStorage.setItem("users", JSON.stringify(users));
 
  alert("Registered successfully");
  loginUI();
 }
 
+
+// Login
 function login(){
  const u = lu.value.trim();
  const p = lp.value.trim();
 
-const found = users.find(
-    x => x.username === u && x.password === btoa(p)
-);
+ const found = users.find(
+     x => x.username === u &&
+     x.password === hash(p)
+ );
 
-
- if(!found) return alert("Invalid credentials");
+ if(!found)
+    return alert("Invalid login");
 
  localStorage.setItem("session", u);
+
+ startSessionTimer();
  dashboardUI(u);
 }
 
+
+// Logout
 function logout(){
  localStorage.removeItem("session");
+
+ if(sessionTimer)
+    clearTimeout(sessionTimer);
+
  location.reload();
 }
 
 
-/* INIT */
-function guard(){
-    const current = localStorage.getItem("session");
+/* ================= SECURITY ================= */
 
-    if(!current){
-        loginUI();
-    } else {
-        dashboardUI(current);
-    }
+// Auto logout after 5 minutes
+function startSessionTimer(){
+
+ if(sessionTimer)
+    clearTimeout(sessionTimer);
+
+ sessionTimer = setTimeout(() => {
+    alert("Session expired — auto logout");
+    logout();
+ }, 1000 * 60 * 5);   // 5 minutes
 }
 
-// Initial load route guard
-guard();
+// Route Guard
+function guard(){
 
+ const user = localStorage.getItem("session");
+
+ if(!user){
+    loginUI();
+ } else {
+    startSessionTimer();
+    dashboardUI(user);
+ }
+}
+
+// Initial load
+guard();
