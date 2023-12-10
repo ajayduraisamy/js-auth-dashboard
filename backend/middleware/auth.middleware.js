75832679a3e8d21c1e94c64e495d65db3b58150b
Ajay Duraisamy <ajayduraisamy@gmail.com>
2023-12-10 13:00:00 +0530

    security: improve token validation middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,16 +1,20 @@
-// middleware/auth.middleware.js
-
 exports.protect = (req, res, next) => {
-  const token = req.headers["authorization"];
+  const auth = req.headers["authorization"];
 
-  if (!token) {
-    return res.status(401).json({ message: "Access denied. No token." });
+  if (!auth) {
+    return res.status(401).json({ message: "Missing auth token" });
   }
 
-  // Temporary demo validation
+  if (!auth.startsWith("Bearer")) {
+    return res.status(401).json({ message: "Invalid token format" });
+  }
+
+  const token = auth.split(" ")[1];
+
   if (token !== "demo-token") {
-    return res.status(401).json({ message: "Invalid token" });
+    return res.status(401).json({ message: "Unauthorized token" });
   }
 
+  req.user = { role: "user" };
   next();
 };
