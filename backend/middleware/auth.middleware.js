bce21be3f40d002d5a755da295abde4f78d70475
Ajay Duraisamy <ajayduraisamy@gmail.com>
2023-12-11 10:00:00 +0530

    security: add jwt auth middleware

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -1,31 +1,30 @@
-// backend/middleware/auth.middleware.js
-
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
 
 exports.protect = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
+  const header = req.headers.authorization;
 
-  if (!authHeader) {
-    return res.status(401).json({ success: false, message: "No token provided" });
+  if (!header) {
+    return res.status(401).json({
+      success: false,
+      message: "No auth token provided",
+    });
   }
 
-  if (!authHeader.startsWith("Bearer ")) {
-    return res
-      .status(401)
-      .json({ success: false, message: "Invalid token format" });
-  }
-
-  const token = authHeader.split(" ")[1];
+  const token = header.startsWith("Bearer ")
+    ? header.split(" ")[1]
+    : header;
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = { email: decoded.email };
     next();
   } catch (err) {
-    console.error("Token error:", err.message);
-    return res.status(401).json({ success: false, message: "Invalid or expired token" });
+    return res.status(401).json({
+      success: false,
+      message: "Token invalid or expired",
+    });
   }
 };
