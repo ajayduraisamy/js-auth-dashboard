c1fb53d0ed57ceaaf1e4c7faddc5c116beb98d42
Ajay Duraisamy <ajayduraisamy@gmail.com>
2023-12-14 10:30:00 +0530

    frontend: add global api error fallback

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -10,3 +10,19 @@ export async function post(endpoint, data, token) {
     body: JSON.stringify(data)
   }).then(res => res.json());
 }
+export async function post(endpoint, data, token) {
+  try {
+    const res = await fetch(`${API}${endpoint}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token ? `Bearer ${token}` : ""
+      },
+      body: JSON.stringify(data)
+    });
+
+    return await res.json();
+  } catch {
+    return { success: false, message: "Network request failed" };
+  }
+}
